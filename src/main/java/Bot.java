4cf6c9bcd14e916d729337f00156fc3a5f5eaf2f
Jacob Huber <jakealanhuber@gmail.com>
2022-07-29 16:45:21 -0400

    Implemented feature for showing users how many hours they've studied today.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -13,6 +13,9 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Bot extends ListenerAdapter {
 
     private MongoClient client;
@@ -33,11 +36,22 @@ public class Bot extends ListenerAdapter {
             Document document = new Document();
             document.append("_id", id);
             document.append("time_studied", 0.0);
+            document.append("time_zone", "UTC");
+            document.append("today_study_times", new Document());
+            document.append("date_created", getDate("UTC"));
             col.insertOne(document);
         }
         query = new BasicDBObject("_id", id);
         user_doc = (Document) col.find(query).first();
         return user_doc;
+    }
+
+    private String getDate(String timeZone) {
+        TimeZone tz = TimeZone.getTimeZone(timeZone);
+        Calendar time = Calendar.getInstance(tz);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM. dd, YYYY", Locale.US);
+        simpleDateFormat.setTimeZone(tz);
+        return simpleDateFormat.format(time.getTime());
     }
 
     @Override
@@ -48,25 +62,40 @@ public class Bot extends ListenerAdapter {
             channel.sendMessage("<@" + event.getAuthor().getId() + ">, hello! I'll ping you in " +
                                 "25 minutes. ").queue();
             try {
-                Thread.sleep(10000);
+                Thread.sleep(5000);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
             channel.sendMessage("Hello <@" + event.getAuthor().getId() + ">! It's been 25 minutes. I'll ping " +
                                 "you in 5 minutes.").queue();
             try {
-                Thread.sleep(10000);
+                Thread.sleep(5000);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
 
-            // Update user's total time studied
+            // Update user's total time studied and adds this study time to the today study time document
             // https://www.mongodb.com/docs/drivers/java/sync/current/usage-examples/updateOne/
-            Document user_doc = getUserById("test2");
+            UpdateOptions options = new UpdateOptions().upsert(true);
+
+            Document user_doc = getUserById("test5");
             double time_studied = (double) user_doc.get("time_studied");
             time_studied += 0.5;
-            Bson updatedDoc = Updates.combine(Updates.set("time_studied", time_studied));
-            UpdateOptions options = new UpdateOptions().upsert(true);
+
+            String todayDate = getDate((String) user_doc.get("time_zone"));
+
+            double studyTimeToday = 0.5;
+            Object prevStudyTimeToday = user_doc.get(todayDate);
+            String lastStudyDate = (String) user_doc.get("last_study_date");
+            if (lastStudyDate != null && lastStudyDate.equals(todayDate)) {
+                if (prevStudyTimeToday != null) {
+                    studyTimeToday += (double) prevStudyTimeToday;
+                }
+            }
+
+            Bson updatedDoc = Updates.combine(Updates.set("time_studied", time_studied),
+                    Updates.set("time_studied_today", studyTimeToday), Updates.set("last_study_date", todayDate));
+
             try {
                 col.updateOne(user_doc, updatedDoc, options);
             } catch (MongoException me) {
@@ -77,8 +106,37 @@ public class Bot extends ListenerAdapter {
         }
         if (msg.getContentRaw().equals("!get-time-studied")) {
             MessageChannel channel = event.getChannel();
-            Document user_doc = getUserById("test2");
-            channel.sendMessage(event.getAuthor().getName() + " time studied: " + user_doc.get("time_studied") + " hours.").queue();
+            Document user_doc = getUserById("test5");
+            channel.sendMessage(event.getAuthor().getName() + ", you've studied for "
+                    + user_doc.get("time_studied") + " hours since " + user_doc.get("date_created") + ".").queue();
+        }
+        if (msg.getContentRaw().equals("!get-time-studied-today")) {
+            MessageChannel channel = event.getChannel();
+            Document user_doc = getUserById("test5");
+            String currentDate = getDate((String) user_doc.get("time_zone"));
+            if (currentDate.equals(user_doc.get("last_study_date"))) {
+                channel.sendMessage(event.getAuthor().getName() + ", you've studied for " +
+                        user_doc.get("time_studied_today") + " hours today.").queue();
+            }
+            else {
+                channel.sendMessage(event.getAuthor().getName() + ", you've studied for 0 hours today.").queue();
+            }
+        }
+
+        // TODO
+        if (msg.getContentRaw().equals("!set-time-zone")) {
+            String[] timeZones = {"GMT", "UTC", "ECT", "EET", "ART", "EAT", "MET", "NET", "PLT", "IST", "BST", "VST", "CTT", "JST",
+            "ACT", "AET", "SST", "NST", "MIT", "HST", "AST", "PST", "PNT", "MST", "CST", "EST", "IET", "PRT", "CNT", "AGT", "BET", "CAT"};
+            String str = "";
+            for (int i = 0; i < timeZones.length - 1; i++) {
+                str += (timeZones[i] + ", ");
+            }
+            str += timeZones[timeZones.length -1];
+
+            MessageChannel channel = event.getChannel();
+            Document user_doc = getUserById("test5");
+            channel.sendMessage(event.getAuthor().getName() + ", your current time zone is "
+                    + user_doc.get("time_zone") + ". Please select one of the following time zones:\n" + str).queue();
         }
     }
 }
